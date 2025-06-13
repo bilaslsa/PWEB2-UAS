55c8deda7792e084f852023ce71f98b42896b042
AhmadRizaMuzakki <rizaahmad934@gmail.com>
2025-06-13 20:00:15 +0700

    change: memasukan yang tertinggal

--- app/Filament/Resources/PeminjamanResource.php
+++ app/Filament/Resources/PeminjamanResource.php
@@ -8,7 +8,11 @@ use Filament\Forms\Form;
 use App\Models\Peminjaman;
 use Filament\Tables\Table;
 use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DatePicker;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\PeminjamanResource\Pages;
@@ -24,7 +28,20 @@ class PeminjamanResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('nama_peminjam')->label('nama'),
+                TextInput::make('ktp_peminjam')->label('nomor ktp'),
+                TextInput::make('keperluan_pinjam')->label('alasan'),
+                DatePicker::make('mulai'),
+                DatePicker::make('selesai'),
+                TextInput::make('biaya')->label('biaya'),
+                Select::make('armada_id')->searchable()->preload()->relationship('armada',  'merk'),
+                Textarea::make('komentar_peminjam'),
+                Select::make('status_pinjam')
+                ->options([
+                    'Pinjam' => 'Pinjam',
+                    'Tersedia' => 'Tersedia',
+                ]),
+
             ]);
     }
 
@@ -47,7 +64,7 @@ class PeminjamanResource extends Resource
             ->actions([
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
-                
+
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
