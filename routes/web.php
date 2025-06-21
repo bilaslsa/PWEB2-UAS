dfc58af79de45298547a254676a3cce8fb8b6654
AhmadRizaMuzakki <rizaahmad934@gmail.com>
2025-06-21 15:02:30 +0700

    feat: role-permission

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,5 @@ Route::get('/', function () {
 
 Route::get('/logout', [AuthenticatedSessionController::class , 'destroy']);
 
-Route::get('/beranda', function () {
-    return view('layouts.dashboard');
-})->middleware(['auth', 'verified'])->name('beranda');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__.'/auth.php';
